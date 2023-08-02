a14ef16062e104a4af98d3915868850da7e78189
Chishtiasdaq <chishtiasdaq@gmail.com>
2023-08-02 10:38:59 +0530

    files uploaded

---
+++ app/src/main/java/com/example/temperatureconvertor/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.temperatureconvertor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private Button cTof,fToc;
+    private TextView result;
+    private EditText enterTemp;
+    double result0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cTof = findViewById(R.id.cTof);
+        fToc = findViewById(R.id.fToc);
+        result = findViewById(R.id.result);
+        enterTemp = findViewById(R.id.enterTemp);
+
+        cTof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                double temp = Double.parseDouble(enterTemp.getText().toString());
+                result0 = (temp * 1.8) + 32;
+                result.setText(String.valueOf(result0));
+            }
+        });
+        fToc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double temp = Double.parseDouble(enterTemp.getText().toString());
+                result0 = (temp - 32) / 1.8;
+                result.setText(String.valueOf(result0));
+
+            }
+        });
+
+    }
+}
